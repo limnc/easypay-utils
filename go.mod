88dcbad82d41d7f4cce8833eedac8595fcc3b233
limnc <nianchinlim@gmail.com>
2025-03-02 18:06:12 +0800

    Fixing event logger RabbitMQ issue : Instance not found

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/limnc/easypay-utils
 go 1.23.6
 
 require github.com/rabbitmq/amqp091-go v1.10.0
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+)
--- logger/event.go
+++ logger/event.go
@@ -8,20 +8,32 @@ import (
 	"github.com/limnc/easypay-utils/rabbitmq"
 )
 
+// Define an interface to allow mocking in tests
+type Publisher interface {
+	Publish(msg rabbitmq.PublishMessage) error
+}
+
 type LoggerEvent struct {
 	serviceName string
-	rabbitMQ    *rabbitmq.RabbitMQ
+	publisher   Publisher
 }
 
-// Register services initializes a logger event instance for a service
-func RegisterService(serviceName string, rabbitMQ *rabbitmq.RabbitMQ) *LoggerEvent {
-	return &LoggerEvent{
-		serviceName: serviceName,
-		rabbitMQ:    rabbitMQ,
+// RegisterService initializes a LoggerEvent instance for a service
+func RegisterService(serviceName string, publisher Publisher) *LoggerEvent {
+	if publisher == nil {
+		log.Fatal("[Error] RegisterService: RabbitMQ instance is null")
 	}
+
+	logger := &LoggerEvent{
+		serviceName: serviceName,
+		publisher:   publisher,
+	}
+
+	log.Printf("[DEBUG] RegisterService: Service %s registered\n", serviceName)
+	return logger
 }
 
-// Logaction logs an event and sends to the "logging" exchange
+// LogAction logs an event and sends it to the "logging" queue
 func (e *LoggerEvent) LogAction(logLevel, message string, metadata map[string]interface{}) {
 	log_ := LogRequest{
 		Service:    e.serviceName,
@@ -30,26 +42,28 @@ func (e *LoggerEvent) LogAction(logLevel, message string, metadata map[string]in
 		CreatedAt:  time.Now().Format(time.RFC3339),
 	}
 
-	//If metadata exists, encode it to JSON
+	// If metadata exists, encode it to JSON
 	if metadata != nil {
 		metaJSON, err := json.Marshal(metadata)
 		if err != nil {
 			log.Printf("error marshalling metadata: %v\n", err)
-
 		} else {
 			log_.RequestBody = string(metaJSON)
 		}
 	}
 
-	//Publish log to rabbitMQ
-	err := e.rabbitMQ.Publish(rabbitmq.PublishMessage{
+	// Publish log to RabbitMQ
+	err := e.publisher.Publish(rabbitmq.PublishMessage{
 		Exchange:   "",
 		RoutingKey: "logging",
 		Body:       log_,
 	})
+
 	if err != nil {
 		log.Printf("error publishing log message: %v\n", err)
 	}
-	//log.Println a message of time: service: service , Loglevel:loglevel, message and it will print to console
-	log.Printf("time: %s, service: %s, logLevel: %s, message: %s\n", log_.CreatedAt, log_.Service, log_.LogLevel, log_.LogMessage)
+
+	// Print log message to console
+	log.Printf("time: %s, service: %s, logLevel: %s, message: %s\n",
+		log_.CreatedAt, log_.Service, log_.LogLevel, log_.LogMessage)
 }
